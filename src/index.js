ec2382f63710a61a512189508b9faf073202bca9
slobodyanik <slobodyanik.vyacheslav@gmail.com>
2017-07-04 19:09:39 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-'use strict';
+var formattedMessage =  $('.message-formatted');
+var messageField = $('form .message-field');
 
-console.log($)
+function formatingText(value) {
+    return value.toLowerCase()
+                .trim()
+                .replace(/\s+/g,' ');
+
+
+}
+
+messageField.on('input', function(){
+   var value = messageField.val();
+   var formattedText = formatingText(value);
+   formattedMessage.text(formattedText);
+})
